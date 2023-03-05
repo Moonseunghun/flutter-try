e6653306968aa1b01c2a6d364dad7c8c724ba345
문승훈 <moonseunghun@munseunghun-ui-MacBookPro.local>
2023-03-05 22:40:24 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:AppBar( title : Text(
+          "App_ Practice"
+        )),
+        body: Container(
+          width: 150, height: 150, color: Colors.green,
+          margin:EdgeInsets.all(20) ,
+          child: Row (
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            children: [
+              Text('phone'),
+              Icon(Icons.star),
+              Icon(Icons.heart_broken),
+              Icon(Icons.star)
+            ]
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child:Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_mail)
+            ],
+          )
+        ),
+
+      )
+    );
+  }
+}
