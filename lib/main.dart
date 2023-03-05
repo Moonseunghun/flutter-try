dfdd1064b69ec00931b9b2f3452c2b5253582256
moonseunghun <mangokkaksae2@gmail.com>
2023-03-06 01:45:38 +0900

    Add: 레이아웃

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,40 @@ class MyApp extends StatelessWidget {
         appBar:AppBar( title : Text(
           "App_ Practice"
         )),
-        body: Container(
-          width: 150, height: 150, color: Colors.green,
-          margin:EdgeInsets.all(20) ,
-          child: Row (
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Row(
+          children: [
+            Container(
+              width: 150, height: 150, color: Colors.green,
+              margin: EdgeInsets.all(20),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-            children: [
-              Text('phone'),
-              Icon(Icons.star),
-              Icon(Icons.heart_broken),
-              Icon(Icons.star)
-            ]
-          ),
+                  children: [
+                    Text('phone'),
+                    Icon(Icons.star),
+                    Icon(Icons.heart_broken),
+                    Icon(Icons.star),
+                    Icon(Icons.access_alarms_outlined)
+                  ]
+
+              ),
+            ),
+            Container(
+              width: 150, height: 150, color: Colors.green,
+              margin:EdgeInsets.all(20) ,
+              child: Row (
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                children: [
+                  Text('phone'),
+                  Icon(Icons.star),
+                  Icon(Icons.heart_broken),
+                  Icon(Icons.star),
+                  Icon(Icons.access_alarms_outlined)
+                ]
+              ),
+            ),
+          ],
         ),
         bottomNavigationBar: BottomAppBar(
           child:Row(
