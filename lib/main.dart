bf6e51242da151c51029e4b8c0dffc82f747065a
moonseunghun <mangokkaksae2@gmail.com>
2023-06-28 18:27:48 +0900

    feat : widget fix

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -31,15 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,11 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -65,50 +42,59 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: Text("App_ Practice")),
-      body: Row(
-        children: [
-          Container(
-            width: 150,
-            height: 150,
-            color: Colors.green,
-            margin: EdgeInsets.all(20),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text('phone'),
-                  Icon(Icons.star),
-                  Icon(Icons.heart_broken),
-                  Icon(Icons.star),
-                  Icon(Icons.access_alarms_outlined)
-                ]),
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            color: Colors.green,
-            margin: EdgeInsets.all(20),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text('phone'),
-                  Icon(Icons.star),
-                  Icon(Icons.heart_broken),
-                  Icon(Icons.star),
-                  Icon(Icons.access_alarms_outlined)
-                ]),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.phone),
-          Icon(Icons.message),
-          Icon(Icons.contact_mail)
-        ],
-      )),
-    ));
+            appBar: AppBar(
+              title: Text("babtong"),
+              backgroundColor: Colors.amber,
+            ),
+            // body: Row(
+            //   children: [
+            //     Container(
+            //       width: 150,
+            //       height: 150,
+            //       color: Colors.green,
+            //       margin: EdgeInsets.all(20),
+            //       child: Row(
+            //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //           children: [
+            //             Text('phone'),
+            //             Icon(Icons.star),
+            //             Icon(Icons.heart_broken),
+            //             Icon(Icons.star),
+            //             Icon(Icons.access_alarms_outlined)
+            //           ]),
+            //     ),
+            //     Container(
+            //       width: 150,
+            //       height: 150,
+            //       color: Colors.green,
+            //       margin: EdgeInsets.all(20),
+            //       child: Row(
+            //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //           children: [
+            //             Text('phone'),
+            //             Icon(Icons.star),
+            //             Icon(Icons.heart_broken),
+            //             Icon(Icons.star),
+            //             Icon(Icons.access_alarms_outlined)
+            //           ]),
+            //     ),
+            //   ],
+            // ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Text('bye'), Text('bye'), Text('bye')],
+              ),
+            )
+            // bottomNavigationBar: BottomAppBar(
+            //     child: Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //   children: [
+            //     Icon(Icons.phone),
+            //     Icon(Icons.message),
+            //     Icon(Icons.contact_mail)
+            //   ],
+            // )),
+            ));
   }
 }
