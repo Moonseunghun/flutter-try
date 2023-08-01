1b8093b4936c22a188e08edc1caedab48c44b817
moonseunghun <mangokkaksae2@gmail.com>
2023-08-01 16:24:17 +0900

    Faet : 이커머스

--- lib/main.dart
+++ lib/main.dart
@@ -1,185 +1,194 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(EcommerceApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Ecommerce App',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String message = '';
+class _HomePageState extends State<HomePage> {
+  final List<Product> _products = []; // 가상 상품 목록
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text("application bar"),
-        centerTitle: true,
-        elevation: 0.0,
-        // leading: IconButton(
-        //   icon: Icon(Icons.menu),
-        //   onPressed: () => {print('clicked menu')},
-        // ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_bag_outlined),
-            onPressed: () => {print('clicked')},
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () => {print('clicked search')},
-          ),
-        ],
-        backgroundColor: Colors.amber[700],
+        title: Text('Ecommerce App'),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('asset/seunghun.jpg'),
-                backgroundColor: Colors.white,
-              ),
-              otherAccountsPictures: <Widget>[
-                CircleAvatar(
-                  backgroundImage: AssetImage('asset/seunghun.jpg'),
-                  backgroundColor: Colors.white,
-                ),
-                // CircleAvatar(
-                //   backgroundImage: AssetImage('asset/seunghun.jpg'),
-                //   backgroundColor: Colors.white,
-                // ),
-              ],
-              accountName: Text('승훈'),
-              accountEmail: Text('mangokkaksae2@gmail.com'),
-              onDetailsPressed: () {
-                setState(() {
-                  message = '망했다';
-                  Text(message);
-                });
+      body: ListView.builder(
+        itemCount: _products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_products[index].name),
+            subtitle: Text('${_products[index].price}원'),
+            onTap: () {
+              _navigateToProductDetails(context, _products[index]);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _navigateToAddProduct(context);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _navigateToProductDetails(BuildContext context, Product product) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ProductDetailsPage(product: product),
+      ),
+    );
+  }
+
+  void _navigateToAddProduct(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddProductPage(),
+      ),
+    );
+    if (result != null && result is Product) {
+      setState(() {
+        _products.add(result);
+      });
+    }
+  }
+}
+
+class ProductDetailsPage extends StatelessWidget {
+  final Product product;
+
+  ProductDetailsPage({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.name),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('${product.price}원'),
+            ElevatedButton(
+              onPressed: () {
+                _deleteProduct(context, product);
               },
-              decoration: BoxDecoration(
-                  color: Colors.red[200],
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40.0),
-                      bottomRight: Radius.circular(40.0))),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: Text('home'),
-              onTap: () {
-                print('clicked home');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text('settings'),
-              onTap: () {
-                print('clicked home');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text('QnA'),
-              onTap: () {
-                print('clicked home');
-              },
-              trailing: Icon(Icons.add),
+              child: Text('제품 삭제'),
             ),
           ],
         ),
       ),
-      // body: Builder(builder: (BuildContext context) => {
-      //   retutn Center(
-      //     child: FlatButton(
-      //       child: Text('Show me ' ,
-      //       style: TextStyle(color: Colors.whit),
-      //       )
-      //     ),
-      //   )
-      // })
-      // body: Row(
-      //   children: [
-      //     Container(
-      //       width: 150,
-      //       height: 150,
-      //       color: Colors.green,
-      //       margin: EdgeInsets.all(20),
-      //       child: Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //           children: [
-      //             Text('phone'),
-      //             Icon(Icons.star),
-      //             Icon(Icons.heart_broken),
-      //             Icon(Icons.star),
-      //             Icon(Icons.access_alarms_outlined)
-      //           ]),
-      //     ),
-      //     Container(
-      //       width: 150,
-      //       height: 150,
-      //       color: Colors.green,
-      //       margin: EdgeInsets.all(20),
-      //       child: Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //           children: [
-      //             Text('phone'),
-      //             Icon(Icons.star),
-      //             Icon(Icons.heart_broken),
-      //             Icon(Icons.star),
-      //             Icon(Icons.access_alarms_outlined)
-      //           ]),
-      //     ),
-      //   ],
-      // ),
-
-      // bottomNavigationBar: BottomAppBar(
-      //     child: Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //   children: [
-      //     Icon(Icons.phone),
-      //     Icon(Icons.message),
-      //     Icon(Icons.contact_mail)
-      //   ],
-      // )),
-    ));
+    );
   }
 
-  FlatButton() {}
+  void _deleteProduct(BuildContext context, Product product) {
+    // 여기에서 실제로 데이터베이스와 통신하여 제품을 삭제하는 로직을 작성해야 합니다.
+    // 예시로 가상 제품 목록에서 제품 삭제
+    Navigator.pop(context, product);
+  }
+}
+
+class AddProductPage extends StatefulWidget {
+  const AddProductPage({super.key});
+
+  @override
+  _AddProductPageState createState() => _AddProductPageState();
+}
+
+class _AddProductPageState extends State<AddProductPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('제품 추가'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: '제품 이름'),
+            ),
+            TextField(
+              controller: _priceController,
+              decoration: InputDecoration(labelText: '제품 가격'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                _addProduct(context);
+              },
+              child: Text('추가'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _addProduct(BuildContext context) {
+    String name = _nameController.text;
+    String priceStr = _priceController.text;
+    int price = int.tryParse(priceStr) ?? 0;
+
+    if (name.isNotEmpty && price > 0) {
+      Product newProduct = Product(name: name, price: price);
+      Navigator.pop(context, newProduct);
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('오류'),
+            content: Text('제품 이름과 가격을 올바르게 입력해주세요.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('확인'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
+
+class Product {
+  final String name;
+  final int price;
+
+  Product({required this.name, required this.price});
 }
